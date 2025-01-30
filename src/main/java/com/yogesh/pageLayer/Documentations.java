ee8cfb4a0ae94f8e59d43a492b9217865292d7c4
Yogesh <yogeshnalepatil@gmail.com>
2025-01-30 11:58:50 +0530

    changed

--- src/main/java/com/yogesh/pageLayer/Documentations.java
+++
@@ -1,7 +0,0 @@
-package com.yogesh.pageLayer;
-
-public class Documentations {
-	
-	// some testing related document is added 
-
-}
--- src/main/java/com/yogesh/pageLayer/Home.java
+++
@@ -1,6 +0,0 @@
-package com.yogesh.pageLayer;
-
-public class Home {
-	//home page is added 
-
-}
